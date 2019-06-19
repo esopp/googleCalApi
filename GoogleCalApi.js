9876384bd7882d1c8eb4c34c682dad1e90398277
Emily <emily@Tonys-MacBook-Pro-3.local>
2019-06-19 19:10:07 -0400

    first commit

---
+++ GoogleCalApi.js
@@ -0,0 +1,121 @@
+
+const {google} = require('googleapis');
+
+
+function GoogleCalendarApi () {
+	this.initialized = false;
+	this.oauth2Client = null;
+    this.url = null;
+    this.token = null;
+    this.calendar = null;
+
+    this.init = function (clientId, clientSecret, redirect_url, done) {
+	    this.oauth2Client = new google.auth.OAuth2(
+			clientId,
+			clientSecret, 
+			redirect_url
+		);
+		this.scopes = ['https://www.googleapis.com/auth/calendar'];
+		this.url = this.oauth2Client.generateAuthUrl({
+			  access_type: 'offline',
+			  scope: this.scopes
+		})
+		//listeners
+		this.oauth2Client.on('tokens', (tokens) => {
+			console.log('token event:', this.token)
+		  if (tokens.refresh_token) {
+		    this.token = tokens.refresh_token;
+		    console.log('refresh: ' + tokens.refresh_token)
+		  }
+		  console.log('auth token: ' + tokens.access_token)
+		})
+		console.log('initialized', this.oauth2Client)
+		this.initialized = true;
+		done('initialized');
+	};
+	/**
+	* @param done: callback function that can handle two params: error and url
+	* 			   -error should be null, or an error message
+	*			   -url should be an address for Google Cloud's authentication process
+	**/
+	this.getAuthUrl = (done) => { this.url ? done(null, this.url) : done('error: url not defined') }
+	this.storeCred = function (code, done) { //code from req.query.code
+		async function cred (auth) {
+			try {
+			console.log('storeCred ping')
+			  const {tokens} = await auth.getToken(code)
+			  auth.setCredentials(tokens)
+			  console.log('tokens', tokens)
+			  done(null, 'authenticated')
+			  } catch (err) {
+			  	console.log('error in cred:', err)
+				      done(err)
+			  }
+		}
+		try { cred(this.oauth2Client) } catch (err) { done(err) }
+		
+	}
+	this.addEvent = function (event, done) {
+		console.log('add event ping')
+		if (this.calendar === null) this.calendar = google.calendar({version: 'v3', auth: this.oauth2Client});
+		try {
+		    this.calendar.events.insert({
+				auth: this.oauth2Client,
+				calendarId: 'primary',
+				resource: event
+		    }, (err, data) => { done(null, data) })
+		} catch (err) {
+			done(err)
+		}
+	}
+	this.deleteEvent = function (id, done) {
+		console.log('ping deleteEvent')
+		if (this.calendar === null) this.calendar = google.calendar({version: 'v3', auth: this.oauth2Client});
+		try {
+			this.calendar.events.delete({
+				auth: this.oauth2Client, 
+				calendarId: 'primary',
+				eventId: id, 
+				sendUpdates: 'none'
+			}, (err, data) => err ? done(err) : done(data))
+		} catch (err) {
+			done(err)
+		}
+	}
+	this.listEvents = function (options, done) {
+		console.log('list events ping')
+		console.log('auth value:', this.oauth2Client)
+			if (this.calendar === null) this.calendar = google.calendar({version: 'v3', auth: this.oauth2Client});
+			try {	
+			    this.calendar.events.list({
+			    calendarId: 'primary',
+			    timeMin: (new Date()).toISOString(),
+			    maxResults: 10,
+			    singleEvents: true,
+			    orderBy: 'startTime',
+			  }, (err, res) => {
+			  	console.log("err", err, "res:",  res)
+			    if (err) return done(err)
+			    const events = res.data.items;
+			    if (events.length) {
+			      console.log('Upcoming 10 events:');
+			      events.map((event, i) => {
+			        const start = event.start.dateTime || event.start.date;
+			        console.log(`${start} - ${event.summary}`);
+				    console.log(event)
+				    done(null, events);
+			      });
+			    } else {
+			      console.log('No upcoming events found.');
+			      done(null, 'no events found')
+			    }
+			  }) } catch (err) {
+			    done(err)
+			}
+    }
+    	    this.test = function (done) { done('hello world!!!!!!!') }
+
+	
+} //end constructor
+
+module.exports = GoogleCalendarApi;
---
+++ test.js
@@ -0,0 +1,71 @@
+"use strict";
+//Google Calendar API module 
+//const fs = require('fs');
+//const readline = require('readline');
+const express = require('express');
+const app = express();
+
+//must be initialized with .init()
+const CalendarManager = require('./GoogleCalApi.js');
+const calendar = new CalendarManager();
+require('dotenv').config('./.env');
+
+const sessionEvents =  [];
+
+console.log(process.env.REDIRECT_URL, Object.keys(calendar))
+calendar.test(console.log)
+calendar.init(process.env.GOOGLE_CLIENT_ID, 
+	process.env.GOOGLE_CLIENT_SECRET, 
+	process.env.REDIRECT_URL + '/oauthcallback', 
+	(m) => console.log);
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/views/test.html')
+})
+app.get('/test', (req, res) => {
+	calendar.test((message) => {
+		res.send(message);
+	})
+})
+app.get('/verify', (req, res) => {
+  calendar.getAuthUrl((err, url)=>{ err ? res.send('error getting auth url') : 
+  	res.redirect(url) });
+})
+
+app.get('/oauthcallback', (req, res) => {
+	console.log('callback ping')
+ let done = (err, message) => message ? res.send(message) : res.send(err);
+ calendar.storeCred(req.query.code, done)
+})
+
+app.get('/list', (req, res) => {
+	console.log('list  route ping')
+	console.log('req', req)
+	console.log('res', res)
+	calendar.listEvents({}, (err, data) => { res.send(err ? err : data)});
+})
+app.get('/add', (req, res) => {
+	console.log('add  route ping')
+	let event = {
+	    'summary': 'Chloe NONBirthday',
+	    'start': {
+	      'date': '2019-07-13'
+	    },
+	    'end': {
+	      'date': '2019-07-14'
+	    }
+    }
+	calendar.addEvent(event, (err, data) => { 
+		if (data.data.id) sessionEvents.push(data.data.id)
+		console.log(sessionEvents)
+		res.send(err ? err : data.data) 
+	});
+})
+app.get('/delete', (req, res) => {
+   if (sessionEvents.length > 0) calendar.deleteEvent(sessionEvents.shift(), (err, data) => {
+   	res.send(err ? err : data) 
+   })
+   	else res.send('no events in local memory')
+})
+
+app.listen(3000, () => {console.log('listening on port 3000')})
